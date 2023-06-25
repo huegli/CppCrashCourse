70592bad61e8786f902d9ddc476e55dd11a7509e
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-06-25 10:02:10 -0700

    Chapter 7 WIP

---
+++ ch7/heap.cpp
@@ -0,0 +1,47 @@
+#include <cstddef>
+#include <new>
+
+struct Bucket {
+  const static size_t data_size{ 4096 };
+  std::byte data[data_size];
+};
+
+struct Heap {
+  void* allocate(size_t bytes) {
+    if (bytes > Bucket::data_size) throw std::bad_alloc{};
+    for (size_t i{}; i < n_heap_buckets; i++) {
+      if (!bucket_used[i]) {
+        bucket_used[i] = true;
+        return buckets[i].data;
+      }
+    }
+    throw std::bad_alloc{};
+  }
+
+  void free(void* p) {
+    for (size_t i{}; i < n_heap_buckets; i++) {
+      if (buckets[i].data == p) {
+        bucket_used[i] = false;
+        return;
+      }
+    }
+  }
+
+  static const size_t n_heap_buckets{ 10 };
+  Bucket buckets[n_heap_buckets]{};
+  bool bucket_used[n_heap_buckets]{};
+};
+
+Heap heap;
+
+void* operator new(size_t n_bytes) {
+  return heap.allocate(n_bytes);
+}
+
+void operator delete(void* p) {
+  return heap.free(p);
+}
+
+int main() {
+    
+}
